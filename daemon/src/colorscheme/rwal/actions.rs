fd9ef372cb245297835efc055d1783c22f839d77
prepodobnuy <zvfvbycsy6@gmail.com>
2025-04-13 21:39:37 +0300

    omg x2

--- daemon/src/colorscheme/rwal/actions.rs
+++ daemon/src/colorscheme/rwal/actions.rs
@@ -1,11 +1,9 @@
 use std::fs;
 use std::path::Path;
 
-use image::imageops::FilterType::Nearest;
-use image::RgbImage;
-
 use crate::expand_user;
-use crate::wallpaper::display::ImageOperations;
+use crate::wallpaper::image::ImageOperations;
+use crate::wallpaper::image::get_thumbed_image;
 use crate::COLORS_PATH;
 
 use super::rwal::get_palette;
@@ -55,34 +53,4 @@ pub fn cache_rwal(
 
     fs::write(color_scheme_path, &pallete).unwrap();
     fs::write(expand_user(COLORS_PATH), &pallete).unwrap();
-}
-
-fn get_thumbed_image(image_path: &str, image_ops: &ImageOperations, w: u32, h: u32) -> RgbImage {
-    if let Ok(mut _image) = image::open(image_path) {
-        _image = _image.resize_exact(w, h, Nearest);
-        if image_ops.contrast != 0.0 {
-            _image = _image.adjust_contrast(image_ops.contrast)
-        }
-        if image_ops.brightness != 0 {
-            _image = _image.brighten(image_ops.brightness)
-        }
-        if image_ops.hue != 0 {
-            _image = _image.huerotate(image_ops.hue)
-        }
-        if image_ops.blur != 0.0 {
-            _image = _image.blur(image_ops.blur)
-        }
-        if image_ops.flip_h {
-            _image = _image.fliph()
-        }
-        if image_ops.flip_v {
-            _image = _image.flipv()
-        }
-        if image_ops.invert {
-            _image.invert()
-        }
-        _image.to_rgb8()
-    } else {
-        RgbImage::new(4, 4)
-    }
 }
